8ab53881783c50f61965c1aae1fb77e925a55d5b
David Schmitz <koenighotze@yahoo.com>
2014-11-16 17:08:38 +0100

    added monitor bean

---
+++ src/main/java/org/koenighotze/jee7hotel/business/monitor/ApplicationMonitorBean.java
@@ -0,0 +1,42 @@
+
+
+package org.koenighotze.jee7hotel.business.monitor;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ws.rs.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author dschmitz
+ */
+@Startup
+@Singleton
+//@Path("monitor")
+public class ApplicationMonitorBean {
+    private static final Logger LOGGER = Logger.getLogger(ApplicationMonitorBean.class.getName());   
+
+    // TODO add Web Socket that publishes state of the union
+
+    @PostConstruct
+    public void startup() {
+        LOGGER.info("Application is running at " + DateTimeFormatter.ISO_DATE_TIME.format(LocalDateTime.now()));
+    }
+
+    @Schedule(persistent = false, minute = "*/1", hour = "*", second = "0")
+    public void watchdog() {
+        LOGGER.info("Application still running");
+    }
+
+    @PreDestroy
+    public void shuttingDown() {
+        LOGGER.info("Application is shutting down at " + DateTimeFormatter.ISO_DATE_TIME.format(LocalDateTime.now()));
+    }
+
+}
