6357baa5ffe02a7e403ee704e8f28f1909ab5419
David Schmitz <david.schmitz.privat@gmail.com>
2015-10-10 15:10:12 +0200

    Integrated facilities and reservation

--- facilities/src/main/java/org/koenighotze/jee7hotel/facilities/frontend/RoomsBean.java
+++ facilities/src/main/java/org/koenighotze/jee7hotel/facilities/frontend/RoomsBean.java
@@ -8,10 +8,15 @@ import org.koenighotze.jee7hotel.framework.application.registry.AddReservationUr
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
+import javax.faces.application.FacesMessage;
+import javax.faces.event.ComponentSystemEvent;
 import javax.inject.Inject;
 import java.util.List;
 
+import static java.lang.String.format;
 import static javax.faces.application.FacesMessage.SEVERITY_WARN;
+import static javax.faces.context.FacesContext.getCurrentInstance;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.koenighotze.jee7hotel.framework.frontend.FacesMessageHelper.addMessage;
 
 /**
@@ -28,6 +33,24 @@ public class RoomsBean {
     @Inject
     @AddReservationUrl
     private String addNewReservationUrl;
+    private String publicGuestId;
+    private String reservationNumber;
+    private String roomNumber;
+
+
+    /**
+     * Adds a message if a reservation is indicated via get parameters
+     *
+     * @param evt the event
+     */
+    public void initReservationMessage(ComponentSystemEvent evt) {
+        if (isBlank(publicGuestId) || isBlank(reservationNumber)) {
+            return;
+        }
+        FacesMessage message = new FacesMessage(format("Room %s is booked for %s; Reservation number %s",
+                roomNumber, publicGuestId, reservationNumber));
+        getCurrentInstance().addMessage(null, message);
+    }
 
     @PostConstruct
     public void init() {
@@ -44,5 +67,29 @@ public class RoomsBean {
 
     public String getAddNewReservationUrl() {
         return addNewReservationUrl;
+    }
+
+    public void setPublicGuestId(String publicGuestId) {
+        this.publicGuestId = publicGuestId;
+    }
+
+    public String getPublicGuestId() {
+        return publicGuestId;
+    }
+
+    public void setReservationNumber(String reservationNumber) {
+        this.reservationNumber = reservationNumber;
+    }
+
+    public String getReservationNumber() {
+        return reservationNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
     }
 }
